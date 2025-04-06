2e3118c8e7dce536dc178bdf913941edc256a68a
ab00bae <esorebube@gmail.com>
2025-04-06 17:50:44 -0400

    binglerun

--- Topic1/showboard.adb
+++ Topic1/showboard.adb
@@ -2,9 +2,9 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 package body ShowBoard is
    procedure Show_Board(Board : in Board_Type; N : in Integer) is
+      Cell_Width : constant Integer := 3;  -- Each cell is 3 characters wide
       Black_Queen : constant String := "♛";
       White_Queen : constant String := "♕";
-      Empty : constant String := " ";
       Horiz : constant String := "─";
       Vert : constant String := "│";
       Top_Left : constant String := "┌";
@@ -16,44 +16,49 @@ package body ShowBoard is
       Bot_Left : constant String := "└";
       Bot_Middle : constant String := "┴";
       Bot_Right : constant String := "┘";
-   begin
-      -- Top border
-      Put(Top_Left);
-      for I in 1 .. N - 1 loop
-         Put(Horiz & Top_Middle);
-      end loop;
-      Put_Line(Horiz & Top_Right);
 
-      -- Rows
+      -- Helper procedure to print horizontal border lines
+      procedure Put_Horiz_Line(Left, Middle, Right : String) is
+      begin
+         Put(Left);
+         for I in 1 .. N - 1 loop
+            for J in 1 .. Cell_Width loop
+               Put(Horiz);
+            end loop;
+            Put(Middle);
+         end loop;
+         for J in 1 .. Cell_Width loop
+            Put(Horiz);
+         end loop;
+         Put_Line(Right);
+      end Put_Horiz_Line;
+
+   begin
+      -- Print top border
+      Put_Horiz_Line(Top_Left, Top_Middle, Top_Right);
+
+      -- Print rows
       for R in 1 .. N loop
          Put(Vert);
          for C in 1 .. N loop
             if Board(R, C) = 1 then
-               Put(" " & Black_Queen & " ");
+               Put(" " & Black_Queen & " ");  -- 3 characters: space, queen, space
             elsif Board(R, C) = 2 then
-               Put(" " & White_Queen & " ");
+               Put(" " & White_Queen & " ");  -- 3 characters: space, queen, space
             else
-               Put(" " & Empty & " ");
+               Put("   ");  -- 3 spaces for empty cell
             end if;
-            if C < N then Put(Vert); end if;
+            Put(Vert);  -- Vertical separator after each cell
          end loop;
-         Put_Line(Vert);
+         Put_Line("");
 
-         -- Middle or bottom border
+         -- Print middle border (if not the last row)
          if R < N then
-            Put(Mid_Left);
-            for I in 1 .. N - 1 loop
-               Put(Horiz & Mid_Middle);
-            end loop;
-            Put_Line(Horiz & Mid_Right);
+            Put_Horiz_Line(Mid_Left, Mid_Middle, Mid_Right);
          end if;
       end loop;
 
-      -- Bottom border
-      Put(Bot_Left);
-      for I in 1 .. N - 1 loop
-         Put(Horiz & Bot_Middle);
-      end loop;
-      Put_Line(Horiz & Bot_Right);
+      -- Print bottom border
+      Put_Horiz_Line(Bot_Left, Bot_Middle, Bot_Right);
    end Show_Board;
 end ShowBoard;
